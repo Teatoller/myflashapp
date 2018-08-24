8472eb983d32eed2fa2f014dc30353b0d2219c0c
Teatoller <steven.otieno1@gmail.com>
2018-08-24 08:49:36 +0300

    Create templates

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a simple blog-like or Diary-like application.
+The user is able to register, login and make posts.
+It is built on Ubuntu Linux using Python and Flask Framework.
+It is built using bootstraps.
+
+
---
+++ data.py
@@ -0,0 +1,32 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum sit amet, consectetur adipisicing elit..',
+            'author': 'Steven Ennis',
+            'post_date': '22-08-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur.',
+            'author': 'Alice Lakwena',
+            'post_date': '23-8-2018',
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur.',
+            'author': 'Hamisi Kombo',
+            'post_date': '23-8-2018',
+        },
+        {
+            'id': 4,
+            'title': 'Article Four',
+            'body': 'Habari za toka hapo Tanga! et, consectetur.',
+            'author': 'Aminata Aisha',
+            'post_date': '24-8-2018',
+        }
+    ]
+    return articles
